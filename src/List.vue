5bfd33b60837fa9e467006458b0f53d7940de3f2
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 14:21:36 +0530

    created list component

--- src/List.vue
+++ src/List.vue
@@ -1,36 +1,22 @@
 <template>
-   <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div>
         <h1>Filter and Mixins</h1>
-        <p >{{text | toUppercase | to-lowercase}}</p>
         <input v-model="filterText">
         <ul>
           <li v-for="fruit in filterFruits"> {{fruit}}</li>
 
         </ul>
-      </div>
-      <hr>
-      <app-list></app-list>
-    </div>
 
     </div>
 </template>
 
 <script>
-import List from './List.vue';
 export default {
   data (){
     return {
-      text: ' This filter and Mixins',
       fruits:['Apple','Banna','custruardApple','Chilly'],
       filterText:''
     };
-  },
-  filters:{
-    toUppercase(value){
-      return value.toUpperCase();
-    }
   },
   computed:{
       filterFruits(){
@@ -38,9 +24,6 @@ export default {
           return element.match(this.filterText);
         });
       }
-  },
-  components:{
-    appList: List
   }
 }
 </script>

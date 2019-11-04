66fec2b79ff8d8ca1e59e915cd6637eb1db0e271
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 11:43:09 +0530

    local filter

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
    <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Built In directives</h1>
-        <p v-text="'This is some text'"></p>
-        <p v-html="'<i>This is dnyanda Kharade</i>'"></p>
+        <h1>Filter and Mixins</h1>
+        <p >{{text | toUppercase}}</p>
+
       </div>
     </div>
     </div>
@@ -12,35 +12,19 @@
 
 <script>
 export default {
-
+  data (){
+    return {
+      text: ' This filter and Mixins'
+    };
+  },
+  filters:{
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>

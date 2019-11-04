2222a2d7d0e84a5cc8457a35509debbca7abc325
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 12:00:41 +0530

    alternative to filter ; computed properties

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filter and Mixins</h1>
         <p >{{text | toUppercase | to-lowercase}}</p>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filterFruits"> {{fruit}}</li>
 
+        </ul>
       </div>
     </div>
     </div>
@@ -14,13 +18,22 @@
 export default {
   data (){
     return {
-      text: ' This filter and Mixins'
+      text: ' This filter and Mixins',
+      fruits:['Apple','Banna','custruardApple','Chilly'],
+      filterText:''
     };
   },
   filters:{
     toUppercase(value){
       return value.toUpperCase();
     }
+  },
+  computed:{
+      filterFruits(){
+        return this.fruits.filter((element)=>{
+          return element.match(this.filterText);
+        });
+      }
   }
 }
 </script>
